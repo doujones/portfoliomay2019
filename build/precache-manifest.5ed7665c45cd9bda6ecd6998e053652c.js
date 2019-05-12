59c437c5a32ab9f9f5d8c14bed51d5a4c0da178d
Dou Jones <MISTERCHIEFEXECUTIVEOFFICER@YAHOO.COM>
2019-05-12 07:06:58 -0400

    Add files via upload

---
+++ build/precache-manifest.5ed7665c45cd9bda6ecd6998e053652c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6237fd4435c9b0a2f1f1811793636d11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18c5d2f63bfd67054967",
+    "url": "/static/js/2.8add8323.chunk.js"
+  },
+  {
+    "revision": "38ffa030bbead22ac5f3",
+    "url": "/static/js/main.e77bef9c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa49ab913553ec5e9d12cf5b7fcd4040",
+    "url": "/static/media/Img0.fa49ab91.jpeg"
+  },
+  {
+    "revision": "97bebbe948eb9da05738c7551d9dfe02",
+    "url": "/static/media/Img1.97bebbe9.jpeg"
+  },
+  {
+    "revision": "4c6e78b383baae5bc5b6ae74a2594a25",
+    "url": "/static/media/img10.4c6e78b3.jpeg"
+  },
+  {
+    "revision": "a481a48fe83265a7ae126d699afb5064",
+    "url": "/static/media/img11.a481a48f.jpeg"
+  },
+  {
+    "revision": "93f5f5ced28f5beba3bda85b3d970c61",
+    "url": "/static/media/img12.93f5f5ce.jpeg"
+  },
+  {
+    "revision": "498afd0cba43d515e27e522f18dfa7ea",
+    "url": "/static/media/img13.498afd0c.jpeg"
+  },
+  {
+    "revision": "4210c7f7c9fc032b12888abc8d4d5275",
+    "url": "/static/media/img14.4210c7f7.jpeg"
+  },
+  {
+    "revision": "1776d1109112038db1e2e04642fdb3d1",
+    "url": "/static/media/img15.1776d110.jpeg"
+  },
+  {
+    "revision": "72259b955b038913f9e7d47b768ae2ae",
+    "url": "/static/media/img16.72259b95.jpeg"
+  },
+  {
+    "revision": "680418c59f917e7f5c9dcd6590fde318",
+    "url": "/static/media/img17.680418c5.jpeg"
+  },
+  {
+    "revision": "0e84205ae33712c566ef5a8d8741116f",
+    "url": "/static/media/img6.0e84205a.jpeg"
+  },
+  {
+    "revision": "d2e73fab08e3f6f3710da8eb5c665ccd",
+    "url": "/static/media/img7.d2e73fab.jpeg"
+  },
+  {
+    "revision": "f461b02b27a3867a27f1e3716731c9b9",
+    "url": "/static/media/img8.f461b02b.jpeg"
+  },
+  {
+    "revision": "22a6477bb63598410640710dbfa11872",
+    "url": "/static/media/img9.22a6477b.jpeg"
+  }
+]);
